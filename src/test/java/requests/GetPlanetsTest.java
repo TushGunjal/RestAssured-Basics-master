cb9d7ed0c0279abeeeeabf8f8b6b01e160a717e9
TushGunjal <tushgunjal.be@gmail.com>
2023-09-02 01:55:37 +0530

    Task 1 Testing for The Star Wars API (SWAPI) is a public API containing facts about the Star Wars universe.

---
+++ src/test/java/requests/GetPlanetsTest.java
@@ -0,0 +1,40 @@
+package requests;
+
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class GetPlanetsTest extends CommonTest {
+
+	@Test
+	public void GetPlanets(){
+
+		Response response = given()
+				.spec(reqSpec)
+				.when()
+				.get(BASE_URL + PLANETS + "/14"+"?format=wookiee")
+				.then()
+				.statusCode(SC_OK)
+				.extract()
+				.response();
+
+		JsonPath json = response.jsonPath();
+
+        //System.out.println("Wookiee Format Planet Response \n"+response.prettyPrint());
+		assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+
+	}
+
+
+
+
+
+}
